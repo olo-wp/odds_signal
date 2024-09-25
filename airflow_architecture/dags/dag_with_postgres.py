d9fc1cdfcfe3a849d3d0f5130464fdba283e7368
olo <olgierd.zygmunt@student.uj.edu.pl>
2024-09-25 13:27:13 +0200

    airflow and postgres connected and working

--- airflow_architecture/dags/dag_with_postgres.py
+++ airflow_architecture/dags/dag_with_postgres.py
@@ -1,5 +1,6 @@
 import sys
 from datetime import timedelta
+from datetime import datetime
 
 from airflow import DAG
 from airflow.operators.python import PythonOperator
@@ -19,6 +20,23 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
+def print_table():
+    pg_hook = PostgresHook(postgres_conn_id=postgres_conn_id)
+    connection = pg_hook.get_conn()
+    cursor = connection.cursor()
+    cursor.execute(
+            """
+            SELECT * FROM games_id
+            JOIN games ON games.id = games_id.id;     
+            """
+                   )
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    connection.close()
+
 
 def scrape_and_insert():
     df = fortuna_scrape.fortuna_scraping()
@@ -26,12 +44,34 @@ def scrape_and_insert():
     pg_hook = PostgresHook(postgres_conn_id=postgres_conn_id)
     connection = pg_hook.get_conn()
     cursor = connection.cursor()
+    now = datetime.now()
 
     for record in df.itertuples(index=False, name=None):
-        cursor.execute(
-            "INSERT INTO odds (game, home, draw, away, game_date) VALUES (%s, %s, %s, %s, %s)",
-            record
-        )
+        game, home, draw, away, game_date = record
+        try:
+            cursor.execute(
+                """
+                INSERT INTO games_id (game, game_date)
+                VALUES (%s, %s) 
+                ON CONFLICT (game, game_date) 
+                DO UPDATE SET game = EXCLUDED.game
+                RETURNING ID
+                """,
+                (game,game_date)
+            )
+
+            game_id = cursor.fetchone()[0]
+
+            cursor.execute(
+                """
+                INSERT INTO games (id,home, draw, away, odds_date)
+                 VALUES (%s,%s, %s, %s, %s)
+                """,
+                (game_id, home, draw, away, now)
+            )
+        except Exception as e:
+            print(f'Failed to insert record: {e}')
+            continue
     connection.commit()
     cursor.close()
     connection.close()
@@ -46,18 +86,30 @@ with DAG(
 ) as dag:
 
 
-    create_table = SQLExecuteQueryOperator(
-        task_id='create_table',
+    create_table_games_id = SQLExecuteQueryOperator(
+        task_id='create_table_games_id',
         conn_id=postgres_conn_id,
         sql="""
-        CREATE TABLE IF NOT EXISTS odds (
-        game varchar,
+        CREATE TABLE IF NOT EXISTS games_id (
+        id SERIAL PRIMARY KEY,
+        game varchar(64),
+        game_date timestamp,
+        UNIQUE (game, game_date)
+        );
+        """
+    )
+
+    create_table_games = SQLExecuteQueryOperator(
+        task_id='create_table_games',
+        conn_id=postgres_conn_id,
+        sql="""
+        CREATE TABLE IF NOT EXISTS games (
+        id int references games_id(id),
         home float,
         draw float,
         away float,
-        game_date timestamp,
-        saved timestamp,
-        PRIMARY KEY (game, game_date)
+        odds_date timestamp,
+        PRIMARY KEY (id,odds_date)
         );
         """
     )
@@ -67,10 +119,9 @@ with DAG(
         python_callable=scrape_and_insert,
     )
 
-    print_postgres = SQLExecuteQueryOperator(
+    print_postgres = PythonOperator(
         task_id='print_postgres',
-        conn_id=postgres_conn_id,
-        sql="select * from odds;"
+        python_callable=print_table,
     )
 
-    create_table >> scrape_and_insert >> print_postgres
+    create_table_games_id >> create_table_games >> scrape_and_insert >> print_postgres
